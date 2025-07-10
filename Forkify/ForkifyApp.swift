d370061123e38ca209dd4b01ab0eec50dc9f88a6
Danie K <41302248+elledkaye@users.noreply.github.com>
2025-07-10 10:38:16 -0400

    First commit
    First commit w/initial project folders

---
+++ Forkify/ForkifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForkifyApp.swift
+//  Forkify
+//
+//  Created by Danielle Kaye on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ForkifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForkifyTests/ForkifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForkifyTests.swift
+//  ForkifyTests
+//
+//  Created by Danielle Kaye on 7/1/25.
+//
+
+import Testing
+@testable import Forkify
+
+struct ForkifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
