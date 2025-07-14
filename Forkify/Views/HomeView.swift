168ff9896ab0b7c6ec19ab5d91d172ae6e5ce3b2
Danie K <41302248+elledkaye@users.noreply.github.com>
2025-07-14 11:33:48 -0400

    Basic HomeView Layout
    Created a simple layout for HomeView, included a title, subtitle and a textfield where the user will enter in text

--- Forkify/Views/HomeView.swift
+++ Forkify/Views/HomeView.swift
@@ -1,11 +1,28 @@
 
 import SwiftUI
 
+/// Welcome screen with a search bar, and display lists
 struct HomeView: View {
+    
+    /// @State variable to track what the user enters in
+    @State private var searchText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16){
+            Text("Welcome To Forkify")
+                .font(.largeTitle.bold())
+            Text("Find recipes for anything in your fridge!")
+                .font(.subheadline)
+                .padding(.horizontal)
+            
+            // We bind the searchText variable
+            TextField("Search for a recipe", text: $searchText )
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal)
+        } // End of VStack
+        
     }
-}
+} // End of struct
 
 #Preview {
     HomeView()
