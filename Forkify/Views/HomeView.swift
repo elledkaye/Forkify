54661c13fe09f73ed855f77ac6c64fc914e68eb3
Danie K <41302248+elledkaye@users.noreply.github.com>
2025-07-17 09:13:04 -0400

    HomeViewModel
    Added in basic logic

--- Forkify/Views/HomeView.swift
+++ Forkify/Views/HomeView.swift
@@ -4,8 +4,12 @@ import SwiftUI
 /// Welcome screen with a search bar, and display lists
 struct HomeView: View {
     
+    
+    @StateObject var viewModel = HomeViewModel()
+    
     /// @State variable to track what the user enters in
     @State private var searchText: String = ""
+    
     
     var body: some View {
         VStack(spacing: 16){
@@ -19,6 +23,14 @@ struct HomeView: View {
             TextField("Search for a recipe", text: $searchText )
                 .textFieldStyle(.roundedBorder)
                 .padding(.horizontal)
+            
+            Button("Search"){
+                // When the user clicks the search button, we will
+                // Call a function to validate if the input if valid (no special characters or just space)
+                // Navigate to a "Recipe list view"
+                // If there are no results we will display "No Results Found"
+                
+            }
         } // End of VStack
         
     }
