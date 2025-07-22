ef8092e528f6261357e743aaa9e747a3e4ab36f3
Danie K <41302248+elledkaye@users.noreply.github.com>
2025-07-22 12:32:12 -0400

    Added validation to HomeView Search Button
    Added validation for "Search" button, when user clicks the "Search" button function searchRecipeText from the HomeViewModel will be triggered

--- Forkify/Views/HomeView.swift
+++ Forkify/Views/HomeView.swift
@@ -4,7 +4,8 @@ import SwiftUI
 /// Welcome screen with a search bar, and display lists
 struct HomeView: View {
     
-    
+    /// Initializes and owns an instance of HomeViewModel using @StateObject,
+    /// so that the view can observe and react to its published changes.
     @StateObject var viewModel = HomeViewModel()
     
     /// @State variable to track what the user enters in
@@ -20,16 +21,23 @@ struct HomeView: View {
                 .padding(.horizontal)
             
             // We bind the searchText variable
-            TextField("Search for a recipe", text: $searchText )
+            TextField("Search for a recipe", text: $viewModel.searchText )
                 .textFieldStyle(.roundedBorder)
                 .padding(.horizontal)
+            
+            /// if viewModel.errorMessage is not empty display the error message, and if it is empty don't display the error message
+            if !viewModel.errorMessage.isEmpty{
+                Text(viewModel.errorMessage)
+                    .foregroundColor(Color.red)
+            }
             
             Button("Search"){
                 // When the user clicks the search button, we will
                 // Call a function to validate if the input if valid (no special characters or just space)
                 // Navigate to a "Recipe list view"
                 // If there are no results we will display "No Results Found"
-                
+                /// Call the searchRecipeText function that is inside the HomeViewModel
+                viewModel.searchRecipeText()
             }
         } // End of VStack
         
