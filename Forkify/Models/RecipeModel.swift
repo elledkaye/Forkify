3715bcc4adbacdc7b3b5e42345a699aade20657e
Danie K <41302248+elledkaye@users.noreply.github.com>
2025-09-07 19:57:27 -0400

    Added 'Services' folder
    Created a 'Services' folder

--- Forkify/Models/RecipeModel.swift
+++ Forkify/Models/RecipeModel.swift
@@ -1,1 +1,11 @@
+/// RecipeModel which matches the Food2Fork API response
 import Foundation
+
+struct RecipeModel: Identifiable, Decodable {
+    let id: String
+    let title: String
+    let publisher: String
+    let image_url: String
+    let rating: String
+    let source_url: String
+}
